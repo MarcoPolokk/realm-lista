26ec0ed022e61f4765cddda698d16f5ec06d25ef
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2020-09-24 14:14:30 +0200

    Data persisted using UserDefaults method

--- realm-lista/Controllers/TableViewController.swift
+++ realm-lista/Controllers/TableViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
+    
+    var itemArray = ["One", "Two", "Three", "Four"]
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "ListArray") as? [String] {
+            itemArray = items
+        }
     }
-
-
+    
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell")!
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //        print(itemArray[indexPath.row])
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new item to the list", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            //What will happen once the user clicks the Add Item button on UIAlert
+            self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ListArray")
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Type new item name"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
-
